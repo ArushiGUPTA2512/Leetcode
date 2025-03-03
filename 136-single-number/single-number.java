9ee4b580c0a372159476ea24fe2648f975300fee
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2025-03-03 19:49:40 +0530

    Time: 1 ms (99.85%) | Memory: 46.2 MB (59.29%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,20 +1,31 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n = nums.length;
-        
-        for (int i = 0; i < n; i++) { // Iterate through all elements, including the last one
-            int num = nums[i];
-            int count = 0;
-            
-            for (int j = 0; j < n; j++) { // Count occurrences of nums[i] in the array
-                if (nums[j] == num)
-                    count++;
-            }
-            
-            if (count == 1)  // If the number appears only once, return it
-                return num;
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;  // XOR all elements
         }
-        
-        return -1;  // Default return value (should never reach here)
+        return result;
     }
 }
+
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         int n = nums.length;
+        
+//         for (int i = 0; i < n; i++) {
+//             int num = nums[i];
+//             int count = 0;
+            
+//             for (int j = 0; j < n; j++) { 
+//                 if (nums[j] == num)
+//                     count++;
+//             }
+            
+//             if (count == 1)  
+//                 return num;
+//         }
+        
+//         return -1;  
+//     }
+// }

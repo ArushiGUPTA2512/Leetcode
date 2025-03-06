1701a43b30a52e1bff195f78b2b6ccc9fc51bbff
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2025-03-06 23:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (65.00%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,25 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.trim().split(" ");
-        return words[words.length - 1].length();
+        int length = 0, i = s.length() - 1;
+        
+        // Ignore trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        
+        // Count the length of the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+        
+        return length;
     }
 }
+
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] words = s.trim().split(" ");
+//         return words[words.length - 1].length();
+//     }
+// }

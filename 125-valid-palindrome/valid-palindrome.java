1883144f792a8cb42d3c0b6b96d757c9e69bb4ce
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2025-04-02 22:22:21 +0530

    Time: 2 ms (99.09%) | Memory: 42.9 MB (83.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,13 +1,37 @@
 class Solution {
-    public boolean isPalindrome(String s) {
-        StringBuilder cleaned = new StringBuilder();
+    public static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
 
-        for (char c : s.toCharArray()) {
-            if (Character.isLetterOrDigit(c)) {
-                cleaned.append(Character.toLowerCase(c));
+        while (left < right) {
+            // Move left pointer forward if it's not alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
             }
+            // Move right pointer backward if it's not alphanumeric
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // Compare characters (convert to lowercase)
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
         }
 
-        return cleaned.toString().equals(cleaned.reverse().toString());
+        return true;
     }
 }
+    // public boolean isPalindrome(String s) {
+    //     StringBuilder cleaned = new StringBuilder();
+
+    //     for (char c : s.toCharArray()) {
+    //         if (Character.isLetterOrDigit(c)) {
+    //             cleaned.append(Character.toLowerCase(c));
+    //         }
+    //     }
+
+    //     return cleaned.toString().equals(cleaned.reverse().toString());
+    // }

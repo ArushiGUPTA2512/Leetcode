6ba6f2ece3865c42827859470fa25f8fa37d01a1
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2025-05-03 21:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (96.40%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,21 +1,19 @@
 class Solution {
     public int numSquares(int n) {
-         int[] dp = new int[n + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        dp[0] = 0;
+        if (isPerfectSquare(n)) return 1;
 
-        List<Integer> squares = new ArrayList<>();
         for (int i = 1; i * i <= n; i++) {
-            squares.add(i * i);
+            if (isPerfectSquare(n - i * i)) return 2;
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int square : squares) {
-                if (i < square) break;
-                dp[i] = Math.min(dp[i], dp[i - square] + 1);
-            }
-        }
+        while (n % 4 == 0) n /= 4;
+        if (n % 8 == 7) return 4;
 
-        return dp[n];
+        return 3;
+    }
+
+    private boolean isPerfectSquare(int n) {
+        int sqrt = (int)Math.sqrt(n);
+        return sqrt * sqrt == n;
     }
 }

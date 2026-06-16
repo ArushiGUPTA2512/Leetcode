b42b68d43062fd4e61a513f6944c736b16f17676
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-06-16 17:09:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (78.52%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,35 +1,37 @@
-class Solution {
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int m,n;
+//         for (int i = 0; i < matrix.length; i++) {
+//     for (int j = 0; j < matrix[i].length; j++) {
+//         if(matrix[i][j]==target)
+//         {
+//             return true;
+//         }
+//         }
+//     }
+//     return false;
+//     }
+// }
+
+class Solution{
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m,n;
-        for (int i = 0; i < matrix.length; i++) {
-    for (int j = 0; j < matrix[i].length; j++) {
-        if(matrix[i][j]==target)
-        {
-            return true;
+        int m = matrix.length, n = matrix[0].length;
+        int left = 0, right = m * n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = matrix[mid / n][mid % n];
+
+            if (midValue == target) {
+                return true;
+            } else if (midValue < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        }
-    }
-    return false;
+
+        return false;
     }
 }
-//        int left = 0, right = m*n- 1;
-//        int result[]=new int[m*n];
-//        int index = 0;
-//         for (int i = 0; i < m; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 result[index++] = matrix[i][j]; // Store element in 1D array
-//             }
-//         }
-//            while (left <= right) {
-//             int mid =( left + right )/ 2;
-
-//             if (result[mid] == target) {
-//                 return true;
-//             } else if (result[mid] < target) {
-//                 left = mid + 1;
-//             } else {
-//                 right = mid - 1;
-//             }
-//         }
     
-//  return false;

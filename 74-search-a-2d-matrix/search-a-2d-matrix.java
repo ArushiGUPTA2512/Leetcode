d98b4ac51dc46e8ee343cdfcf1082c080b7b13fd
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-06-18 21:48:26 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (8.46%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -13,25 +13,43 @@
 //     }
 // }
 
-class Solution{
+// class Solution{
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int m = matrix.length, n = matrix[0].length;
+//         int left = 0, right = m * n - 1;
+
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
+//             int midValue = matrix[mid / n][mid % n];
+
+//             if (midValue == target) {
+//                 return true;
+//             } else if (midValue < target) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//             }
+//         }
+
+//         return false;
+//     }
+// }
+    
+class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int m = matrix.length, n = matrix[0].length;
-        int left = 0, right = m * n - 1;
+        int m = matrix.length;
+        int n = matrix[0].length;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            int midValue = matrix[mid / n][mid % n];
+        int row = 0;
+        int col = n - 1;
 
-            if (midValue == target) {
-                return true;
-            } else if (midValue < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+        while (row < m && col >= 0) {
+            int val = matrix[row][col];
+            if (val == target) return true;
+            else if (val > target) col--;   
+            else row++;                    
         }
 
         return false;
     }
 }
-    

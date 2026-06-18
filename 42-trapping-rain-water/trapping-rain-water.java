c14c846ca777c3b8ad3f2a94bfc0cc83e404e8f6
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-06-19 00:12:15 +0530

    Time: 1 ms (59.41%) | Memory: 47.9 MB (44.95%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,43 @@
-class Solution {
-    public int trap(int[] height) {
+class Solution{
+    public int trap(int[] height){
         int n = height.length;
-        if(n == 0)
-            return 0;
-        int left_max[] = new int[n];
-        left_max[0] = height[0];
-        for(int i=1; i<n; i++)
-            left_max[i] = Math.max(left_max[i-1], height[i]);
-        
-        int right_max[] = new int[n];
-        right_max[n-1] = height[n-1];
-        for(int i=n-2; i>=0; i--)
-            right_max[i] = Math.max(right_max[i+1], height[i]);
-        
+        int lmax = height[0], rmax = height[n-1];
+        int low = 1, high = n-2;
         int ans = 0;
-        for(int i=1; i<n-1; i++)
-            ans += Math.min(left_max[i], right_max[i])-height[i];
+        while(low <= high){
+            lmax = Math.max(lmax, height[low]);
+            rmax = Math.max(rmax, height[high]);
+            if(lmax < rmax){
+                ans += lmax - height[low];
+                low++;
+            }
+            else{
+                ans += rmax - height[high];
+                high--;
+            }
+        }
         return ans;
     }
 }
+
+// class Solution {
+//     public int trap(int[] height) {
+//         int n = height.length;
+//         if(n == 0)
+//             return 0;
+//         int left_max[] = new int[n];
+//         left_max[0] = height[0];
+//         for(int i=1; i<n; i++)
+//             left_max[i] = Math.max(left_max[i-1], height[i]);
+        
+//         int right_max[] = new int[n];
+//         right_max[n-1] = height[n-1];
+//         for(int i=n-2; i>=0; i--)
+//             right_max[i] = Math.max(right_max[i+1], height[i]);
+        
+//         int ans = 0;
+//         for(int i=1; i<n-1; i++)
+//             ans += Math.min(left_max[i], right_max[i])-height[i];
+//         return ans;
+//     }
+// }

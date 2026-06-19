52c2a463b2c409e2b4431acd461d7ed4781f8fba
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-06-19 17:03:05 +0530

    Time: 4 ms (82.16%) | Memory: 44.1 MB (94.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,59 @@
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+class Solution{
+    public boolean isAnagram(String s, String t){
+        int n1 = s.length();
+        int n2 = t.length();
+        if(n1 != n2)
             return false;
+        int count[] = new int[26];
+        for(int i=0; i<n1; i++){
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
         }
-        
-        char[] sArray = s.toCharArray();
-        char[] tArray = t.toCharArray();
-        
-        Arrays.sort(sArray);
-        Arrays.sort(tArray);
-        
-        return Arrays.equals(sArray, tArray);
+        for(int i=0; i<26; i++){
+            if(count[i] != 0)
+                return false;
+        }
+        return true;
     }
 }
+
+// class Solution {
+//    public boolean isAnagram(String s, String t) {
+//        int n1 = s.length();
+//        int n2 = t.length();
+//        if (n1 != n2) return false;
+
+
+//        HashMap<Character, Integer> m1 = new HashMap<>();
+//        HashMap<Character, Integer> m2 = new HashMap<>();
+
+
+//        for (char ch : s.toCharArray()) {
+//            m1.put(ch, m1.getOrDefault(ch, 0) + 1);
+//        }
+
+
+//        for (char ch : t.toCharArray()) {
+//            m2.put(ch, m2.getOrDefault(ch, 0) + 1);
+//        }
+
+
+//        return m1.equals(m2);
+//    }
+// }
+
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if (s.length() != t.length()) {
+//             return false;
+//         }
+        
+//         char[] sArray = s.toCharArray();
+//         char[] tArray = t.toCharArray();
+        
+//         Arrays.sort(sArray);
+//         Arrays.sort(tArray);
+        
+//         return Arrays.equals(sArray, tArray);
+//     }
+// }

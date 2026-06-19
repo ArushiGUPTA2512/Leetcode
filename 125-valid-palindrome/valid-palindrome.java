24a1b2a950fe73dbd511b491fc3a8c8012a0df73
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-06-19 16:48:26 +0530

    Time: 3 ms (60.63%) | Memory: 44.2 MB (75.27%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,37 +1,59 @@
-class Solution {
-    public static boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-
-        while (left < right) {
-            // Move left pointer forward if it's not alphanumeric
-            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+class Solution{
+    public static boolean isPalindrome(String s){
+        int left = 0, right = s.length()-1;
+        while(left < right){
+            if(!Character.isLetterOrDigit(s.charAt(left)))
                 left++;
-            }
-            // Move right pointer backward if it's not alphanumeric
-            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+            else if(!Character.isLetterOrDigit(s.charAt(right)))
+                right--;
+            else if(Character.toLowerCase(s.charAt(left)) == Character.toLowerCase(s.charAt(right))){
+                left++;
                 right--;
             }
-
-            // Compare characters (convert to lowercase)
-            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+            else{
                 return false;
             }
-
-            left++;
-            right--;
         }
-
         return true;
     }
 }
+
+// class Solution {
+//     public static boolean isPalindrome(String s) {
+//         int left = 0, right = s.length() - 1;
+
+//         while (left < right) {
+//             while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+//                 left++;
+//             }
+//             while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+//                 right--;
+//             }
+
+//             if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+//                 return false;
+//             }
+
+//             left++;
+//             right--;
+//         }
+
+//         return true;
+//     }
+// }
+
+
     // public boolean isPalindrome(String s) {
-    //     StringBuilder cleaned = new StringBuilder();
+    //    int n = s.length();
+    //    String str1 = "";
+    //    for (char ch : s.toCharArray()) {
+    //        if (Character.isLetterOrDigit(ch)) {
+    //            str1 += Character.toLowerCase(ch);
+    //        }
+    //    }
+    //    String str2 = str1;
+    //    StringBuilder sb = new StringBuilder(str1);
+    //    str1 = sb.reverse().toString();
 
-    //     for (char c : s.toCharArray()) {
-    //         if (Character.isLetterOrDigit(c)) {
-    //             cleaned.append(Character.toLowerCase(c));
-    //         }
-    //     }
-
-    //     return cleaned.toString().equals(cleaned.reverse().toString());
+    //    return str1.equals(str2);
     // }

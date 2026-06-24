abdb3d81c1ae5597d0ad973f9b3a60f6dc109639
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-06-24 16:52:50 +0530

    Time: 0 ms (100.00%) | Memory: 79.9 MB (96.44%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,18 +1,37 @@
 class Solution {
-    public int peakIndexInMountainArray(int[] arr) {
-        int n = arr.length;
-        int start=0;
-        int end = n-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(mid!=0 && mid!=n-1 && arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
-                return mid;
-            }
-            else if(mid!=n-1 && arr[mid]<arr[mid+1])
-                start = mid+1;
-            else
-                end = mid-1;
-        }
-        return -1;
-    }
+   public int peakIndexInMountainArray(int[] arr) {
+       int low = 0;
+       int high = arr.length - 1;
+      
+       while (low < high)
+       {
+           int mid = (low + high) / 2;
+           if (arr[mid] < arr[mid + 1]) {
+               low = mid + 1;
+           } else {
+               high = mid;
+           }
+       }
+       return low;
+   }
 }
+
+
+// class Solution {
+//     public int peakIndexInMountainArray(int[] arr) {
+//         int n = arr.length;
+//         int start=0;
+//         int end = n-1;
+//         while(start<=end){
+//             int mid = start+(end-start)/2;
+//             if(mid!=0 && mid!=n-1 && arr[mid]>arr[mid+1] && arr[mid]>arr[mid-1]){
+//                 return mid;
+//             }
+//             else if(mid!=n-1 && arr[mid]<arr[mid+1])
+//                 start = mid+1;
+//             else
+//                 end = mid-1;
+//         }
+//         return -1;
+//     }
+// }

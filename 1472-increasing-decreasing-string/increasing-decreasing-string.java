6ea28db74178d84e4875f84408f1fcc0dee10fe8
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-07-24 22:12:47 +0530

    Time: 2 ms (100.00%) | Memory: 45.2 MB (23.31%) - LeetSync

---
+++ 1472-increasing-decreasing-string/increasing-decreasing-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String sortString(String s) {
+       int[] freq = new int[26];
+
+        // Count frequency of each character
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+        StringBuilder ans = new StringBuilder();
+        while (ans.length() < s.length()) {
+            // Smallest to largest
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] > 0) {
+                    ans.append((char)(i + 'a'));
+                    freq[i]--;
+                }
+            }
+
+            // Largest to smallest
+            for (int i = 25; i >= 0; i--) {
+                if (freq[i] > 0) {
+                    ans.append((char)(i + 'a'));
+                    freq[i]--;
+                }
+            }
+        }
+
+        return ans.toString(); 
+    }
+}

25ca90d7cc68c05b92d24157f5632baa60e29886
ArushiGUPTA2512 <arushi809gupta@gmail.com>
2026-08-05 18:23:06 +0530

    Time: 2 ms (26.19%) | Memory: 43.1 MB (17.18%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -0,0 +1,79 @@
+class MyStack {
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q2.add(x);
+        while(!q1.isEmpty()){
+            q2.add(q1.peek());
+            q1.remove();
+        }
+        Queue<Integer> t = q1;
+        q1=q2;
+        q2 = t;
+    }
+    
+    public int pop() {
+        if(q1.isEmpty())
+            return -1;
+        return q1.remove();
+    }
+    
+    public int top() {
+        return q1.peek();
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+
+/*import java.util.*;
+
+class MyStack {
+
+    Queue<Integer> q;
+
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+
+    public void push(int x) {
+
+        q.offer(x);
+
+        int size = q.size();
+
+        while (size > 1) {
+            q.offer(q.poll());
+            size--;
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}*/
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */

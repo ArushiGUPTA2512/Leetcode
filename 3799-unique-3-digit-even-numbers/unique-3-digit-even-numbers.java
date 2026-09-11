6d611e6ac994a5397f02304ad6455138f98a9f12
Arushi Gupta <arushi809gupta@gmail.com>
2026-09-11 22:17:02 +0530

    Time: 10 ms (24.00%) | Memory: 46.7 MB (16.93%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+        int[] freq = new int[10];
+        for (int d : digits) {
+            freq[d]++;
+        }
+
+        int count = 0;
+        for (int a = 1; a <= 9; a++) {
+            for (int b = 0; b <= 9; b++) {
+                for (int c = 0; c <= 8; c += 2) {
+                    int[] used = new int[10];
+
+                    used[a]++;
+                    used[b]++;
+                    used[c]++;
+
+                    boolean possible = true;
+                    for (int d = 0; d <= 9; d++) {
+                        if (used[d] > freq[d]) {
+                            possible = false;
+                            break;
+                        }
+                    }
+                    if (possible) {
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
